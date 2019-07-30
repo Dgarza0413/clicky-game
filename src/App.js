edef758f24db7590f347f2e4441a28d03ae2b467
David Garza <dgarza0413@gmail.com>
2019-07-29 21:31:55 -0500

    pictures added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,35 @@
 // our imports from react as well as the components in use
 import React from 'react';
 // import Header from './components/Header'
-import Board from './components/Board'
-// import Square from './components/Square';
+// import Board from './components/Board'
+import Square from './components/Square';
 
 import './App.css';
+import icons from './icons.json';
 
 // we declare our class App react.component as our parent component
 class App extends React.Component {
+  state = { icons }
+
+  clickSquare = () => {
+    (!this.state.value)
+      ? this.setState({ value: true })
+      : this.setState({ value: false })
+    console.log(`the square was clicked to ${this.state.value}`)
+  }
+
+
   render() {
     return (
       <div className="game">
         <div className="game-board">
-          <Board />
+          {
+            this.state.icons.map(i =>
+              <Square
+                iconImage={i.iconImage}
+              />
+            )
+          }
         </div>
         <div className="game-info">
           <div></div>
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,20 +1,13 @@
 import React from "react"
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            value: null,
-        }
-    }
-    render() {
-        return (
-            <div className="square"
-                onClick={() => this.setState({ value: "X" })}>
-                {this.state.value}
-            </div>
-        )
-    };
+function Square(props) {
+
+
+    return (
+        <div className="square" onClick={() => props.clickSquare()}>
+            <img alt="" src={props.iconImage} />
+        </div>
+    )
 }
 
 export default Square
