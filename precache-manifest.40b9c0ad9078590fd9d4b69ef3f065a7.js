14e250561fab4514475a498952d2f2da880b861e
David Garza <dgarza0413@gmail.com>
2019-07-30 23:16:15 -0500

    Updates

---
+++ precache-manifest.40b9c0ad9078590fd9d4b69ef3f065a7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "012b377418a375d53bbb20021bab7bb0",
+    "url": "/clicky-game/index.html"
+  },
+  {
+    "revision": "0319e5127894bfc96bbf",
+    "url": "/clicky-game/static/css/main.fff3768d.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "0319e5127894bfc96bbf",
+    "url": "/clicky-game/static/js/main.c1ec3b9a.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  }
+]);
